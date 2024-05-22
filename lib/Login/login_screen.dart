1bce269291c188b1a118edc831f2da999a7c34c4
Nick <kkkygsos@naver.com>
2024-05-22 11:54:25 +0900

    설명 주석 추가

--- lib/Login/login_screen.dart
+++ lib/Login/login_screen.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 import '../home/home_screen.dart';  // 상대 경로로 정확하게 import
 
+//StatefulWidget은 상태를 가지는 위젯을 정의할 때 사용됩니다.
 class LoginScreen extends StatefulWidget {
   @override
+  /*
+  createState 메서드는 StatefulWidget에서 반드시 구현해야 하는 메서드로, State 객체를 반환합니다.
+  여기서는 _LoginScreenState 객체를 반환합니다.
+  반환타입 메서드() => 생성객체
+  */
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  /*
+  final 키워드는 해당 변수가 한 번만 할당될 수 있음을 의미
+  _formKey는 변수를 식별하는 이름입니다. 앞에 밑줄(_)이 붙어 있어 이 변수가 프라이빗(private)임을 나타냅니다.
+  이는 이 변수에 접근할 수 있는 범위가 해당 클래스 내로 제한됨을 의미합니다.
+  GlobalKey는 Flutter에서 특정 위젯의 상태에 접근할 수 있게 해주는 키로
+  Form 위젯의 상태에 접근하고 조작하기 위해 사용
+  폼의 상태를 관리하기 위한 GlobalKey를 생성합니다.
+  = FormState 객체를 식별할 수 있는 고유한 키를 생성
+  */
   final _formKey = GlobalKey<FormState>();
+  // 이메일과 비밀번호를 저장할 변수를 선언합니다.
   String _email = '';
   String _password = '';
 
+  // 폼 제출을 처리하는 메서드입니다.
   void _submit() {
+    // 폼이 유효한지 검사합니다. '!'는 null이 아님을 보장
     if (_formKey.currentState!.validate()) {
+      // 폼의 상태를 저장합니다.
       _formKey.currentState!.save();
       // 로그인 로직이나 상태 관리를 처리할 부분
+      // 로그인 성공 시 홈 화면으로 이동합니다.
+      // pushReplacement 메서드는 현재 화면을 새로운 화면으로 대체합니다. 즉, 새로운 화면으로 전환하고 이전 화면을 제거
       Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
+        context, // context는 현재 위젯 트리에서 이 위젯의 위치를 나타내는 BuildContext입니다. 화면 전환 시 필요한 정보임
+        MaterialPageRoute(builder: (context) => HomeScreen()), // MaterialPageRoute는 화면 전환 애니메이션과 함께 새로운 페이지를 생성
       );
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,29 @@ import 'package:flutter/material.dart';
 import 'Login/login_screen.dart';
 
 void main() {
+  // runApp 함수는 주어진 위젯을 화면에 그립니다. 여기서는 MyApp 위젯을 그립니다.
   runApp(MyApp());
 }
 
+// MyApp 클래스는 StatelessWidget을 상속받음
+// extends 키워드를 사용하여 상속을 구현
+// StatelessWidget은 상태가 변하지 않는 위젯을 정의할 때 사용
 class MyApp extends StatelessWidget {
   const MyApp();
 
+  // build 메서드는 위젯의 UI를 구성하는데 사용
   @override
+  // 이 메서드는 StatelessWidget 또는 StatefulWidget 클래스에 있던 기존 build 함수를 자식함수 MyApp에서 재정의
+  // 반환 타입이 Widget
+  // BuildContext는 현재 위젯의 위치와 관련된 메타데이터를 제공
   Widget build(BuildContext context) {
+    // MaterialApp은 Flutter 애플리케이션의 기본적인 설정을 제공
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', // 제목
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // 테마 설정, primarySwatch는 주 색상
       ),
-      home: LoginScreen(),
+      home: LoginScreen(), // home 속성은 애플리케이션이 시작될 때 처음으로 보여줄 화면을 설정
     );
   }
 }
